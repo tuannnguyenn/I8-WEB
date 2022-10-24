cfa76332d9f6f357bdd224ee9e6b372aa37619c8
unknown <minhchien77777@gmail.com>
2022-10-24 15:01:47 +0700

    update 24/10

--- src/main/java/com/i8web/controller/Admin/LoginController.java
+++ src/main/java/com/i8web/controller/Admin/LoginController.java
@@ -1,11 +1,14 @@
 package com.i8web.controller.Admin;
 import javax.servlet.http.HttpSession;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 import com.i8web.entity.Admin.UserAccount;
 
@@ -16,21 +19,22 @@ public class LoginController {
 		ModelAndView mav = new ModelAndView("admin/login/login");
 		return mav;
 	}
-//	
-//	public static class LoginState{
-//		public static boolean isLogin = false;
-//	}
-//	
-//	@RequestMapping(value = "/admin/login", method = RequestMethod.POST)
-//	public  ModelAndView loginPage1(@ModelAttribute(value = "account") UserAccount acc, ModelMap mm, HttpSession session) {
-//		mm.put("account", acc);
-//		if(acc.get_userName().equals("mchien") && acc.get_passWord().equals("mchien")) {
-//			session.setAttribute("_userName", acc.get_userName());
-//			ModelAndView mav = new ModelAndView("home/home");
-//			return mav;
-//		}else {
-//			ModelAndView mav = new ModelAndView("admin/login/login");
-//			return mav;	
-//		}
-//	}
+
+	public static class LoginState{
+		public static boolean isLogin = false;
+	}
+	
+	@RequestMapping(value = "/checkLogin", method = RequestMethod.POST)
+	public ModelAndView checkLogin(@RequestParam("username")String username, @RequestParam("password")String password) {
+		UserAccount userAccount = new UserAccount();
+		userAccount.set_userName("mchien");
+		userAccount.set_passWord("mchien");
+		if (userAccount.get_userName().equals(username) && userAccount.get_passWord().equals(password)) {
+			ModelAndView mav = new ModelAndView("home/home");
+			return mav;
+		} else {
+			ModelAndView mav = new ModelAndView("admin/login/login");
+			return mav;
+		}
+	}
 }
--- src/main/webapp/WEB-INF/views/admin/login/login.jsp
+++ src/main/webapp/WEB-INF/views/admin/login/login.jsp
@@ -21,9 +21,9 @@
     </div>
 
     <!-- Login Form -->
-    <form>
-      <input type="text" id="login" class="fadeIn second" name="login" placeholder="login">
-      <input type="text" id="password" class="fadeIn third" name="login" placeholder="password">
+    <form action="../checkLogin" method="post">
+      <input type="text" id="login" class="fadeIn second" name="username" placeholder="login">
+      <input type="text" id="password" class="fadeIn third" name="password" placeholder="password">
       <input type="submit" class="fadeIn fourth" value="Log In">
     </form>
 
