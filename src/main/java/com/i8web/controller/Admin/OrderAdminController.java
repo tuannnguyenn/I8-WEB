7859c3a8902bfbd834b17911a09eada5c98d183f
01685829762 <tui.hrv@gmail.com>
2022-11-17 21:04:57 +0700

    fix order v1

--- src/main/java/com/i8web/controller/Admin/OrderAdminController.java
+++ src/main/java/com/i8web/controller/Admin/OrderAdminController.java
@@ -1,10 +1,6 @@
 package com.i8web.controller.Admin;
 
-<<<<<<< HEAD
 import javax.servlet.http.HttpServletRequest;
-=======
-import javax.servlet.http.HttpSession;
->>>>>>> chienVer2
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -22,28 +18,14 @@ public class OrderAdminController {
 	OrderServiceImpl orderServiceImpl;
 
 	@RequestMapping(value = "/admin/order/list", method = RequestMethod.GET)
-	public ModelAndView list(HttpSession session) {
-		if (session.getAttribute("adminAccount") == null) {
-			ModelAndView mav = new ModelAndView("admin/register/login");
-			mav.addObject("isError", false);
-			return mav;
-		}
+	public ModelAndView list() {
 		ModelAndView mav = new ModelAndView("/admin/order/list");
 		mav.addObject("orders", orderModel.getData());
 		return mav;
 	}
 	
 	@RequestMapping(value = "/admin/order/detail", method = RequestMethod.GET)
-<<<<<<< HEAD
 	public ModelAndView detail(HttpServletRequest req) {
-=======
-	public ModelAndView detail(HttpSession session) {
-		if (session.getAttribute("adminAccount") == null) {
-			ModelAndView mav = new ModelAndView("admin/register/login");
-			mav.addObject("isError", false);
-			return mav;
-		}
->>>>>>> chienVer2
 		ModelAndView mav = new ModelAndView("/admin/order/detail");
 		String id = req.getParameter("id");
 		System.out.println(id);
